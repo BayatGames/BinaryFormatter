50e733823a659b12c3ef62e847438db9b8c72472
EmpireWorld <hasanbayat1393@gmail.com>
2017-09-11 21:56:54 +0430

    Init
    Initialize Scripts and Unity Project

---
+++ Assets/BayatGames/BinaryFormatter/Scripts/BinaryFormatter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace BayatGames.Serialization.Formatters.Binary
+{
+
+	public class BinaryFormatter
+	{
+
+		#region Fields
+
+		/// <summary>
+		/// The surrogate selector.
+		/// </summary>
+		protected ISurrogateSelector m_SurrogateSelector;
+
+		/// <summary>
+		/// The context.
+		/// </summary>
+		protected StreamingContext m_Context;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the surrogate selector.
+		/// </summary>
+		/// <value>The surrogate selector.</value>
+		public virtual ISurrogateSelector surrogateSelector
+		{
+			get
+			{
+				return m_SurrogateSelector;
+			}
+			set
+			{
+				m_SurrogateSelector = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the context.
+		/// </summary>
+		/// <value>The context.</value>
+		public virtual StreamingContext context
+		{
+			get
+			{
+				return m_Context;
+			}
+			set
+			{
+				m_Context = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Json.JsonFormatter"/> class.
+		/// </summary>
+		public BinaryFormatter () : this ( null, new StreamingContext ( StreamingContextStates.All ) )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Json.JsonFormatter"/> class.
+		/// </summary>
+		/// <param name="selector">Selector.</param>
+		/// <param name="context">Context.</param>
+		public BinaryFormatter ( ISurrogateSelector selector, StreamingContext context )
+		{
+			m_SurrogateSelector = selector;
+			m_Context = context;
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Serialize the specified output and value.
+		/// </summary>
+		/// <param name="output">Output.</param>
+		/// <param name="value">Value.</param>
+		public void Serialize ( Stream output, object value )
+		{
+			using ( BinaryObjectWriter writer = new BinaryObjectWriter ( output, m_SurrogateSelector, m_Context ) )
+			{
+			}
+		}
+
+		/// <summary>
+		/// Deserialize the specified input.
+		/// </summary>
+		/// <param name="input">Input.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public object Deserialize ( Stream input, Type type )
+		{
+			using ( BinaryObjectReader reader = new BinaryObjectReader ( input, m_SurrogateSelector, m_Context ) )
+			{
+				return reader.Read ( type );
+			}
+		}
+
+		#endregion
+		
+	}
+
+}
---
+++ Assets/BayatGames/BinaryFormatter/Scripts/BinaryObjectReader.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+namespace BayatGames.Serialization.Formatters.Binary
+{
+
+	public class BinaryObjectReader : IDisposable
+	{
+
+		#region Fields
+
+		/// <summary>
+		/// The reader.
+		/// </summary>
+		protected BinaryReader m_Reader;
+
+		/// <summary>
+		/// The surrogate selector.
+		/// </summary>
+		protected ISurrogateSelector m_SurrogateSelector;
+
+		/// <summary>
+		/// The context.
+		/// </summary>
+		protected StreamingContext m_Context;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the reader.
+		/// </summary>
+		/// <value>The reader.</value>
+		public virtual BinaryReader reader
+		{
+			get
+			{
+				return m_Reader;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the surrogate selector.
+		/// </summary>
+		/// <value>The surrogate selector.</value>
+		public virtual ISurrogateSelector surrogateSelector
+		{
+			get
+			{
+				return m_SurrogateSelector;
+			}
+			set
+			{
+				m_SurrogateSelector = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the context.
+		/// </summary>
+		/// <value>The context.</value>
+		public virtual StreamingContext context
+		{
+			get
+			{
+				return m_Context;
+			}
+			set
+			{
+				m_Context = value;
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> class.
+		/// </summary>
+		/// <param name="stream">Stream.</param>
+		public BinaryObjectReader ( Stream stream ) : this ( stream, null, new StreamingContext ( StreamingContextStates.All ) )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> class.
+		/// </summary>
+		/// <param name="reader">Reader.</param>
+		public BinaryObjectReader ( BinaryReader reader ) : this ( reader, null, new StreamingContext ( StreamingContextStates.All ) )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> class.
+		/// </summary>
+		/// <param name="stream">Stream.</param>
+		/// <param name="selector">Selector.</param>
+		/// <param name="context">Context.</param>
+		public BinaryObjectReader ( Stream stream, ISurrogateSelector selector, StreamingContext context ) : this ( new BinaryReader ( stream ), selector, context )
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> class.
+		/// </summary>
+		/// <param name="reader">Reader.</param>
+		/// <param name="selector">Selector.</param>
+		/// <param name="context">Context.</param>
+		public BinaryObjectReader ( BinaryReader reader, ISurrogateSelector selector, StreamingContext context )
+		{
+			m_Reader = reader;
+			m_SurrogateSelector = selector;
+			m_Context = context;
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Read the specified type.
+		/// </summary>
+		/// <param name="type">Type.</param>
+		public virtual object Read ( Type type )
+		{
+			object result = null;
+			if ( type == null )
+			{
+				result = null;
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		/// <filterpriority>2</filterpriority>
+		/// <remarks>Call <see cref="Dispose"/> when you are finished using the
+		/// <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/>. The <see cref="Dispose"/> method leaves the
+		/// <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> in an unusable state. After calling
+		/// <see cref="Dispose"/>, you must release all references to the
+		/// <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> so the garbage collector can reclaim the memory
+		/// that the <see cref="BayatGames.Serialization.Formatters.Binary.BinaryObjectReader"/> was occupying.</remarks>
+		public virtual void Dispose ()
+		{
+		}
+
+		#endregion
+		
+	}
+
+}
